fd187e02199c8217bafa64cebbd58cb3029a6473
Aaron Lal <lucidlear@gmail.com>
2024-01-20 19:18:50 +1100

    finished up for the sat

--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import './Admin.css'
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import "./Admin.css";
+import AddProduct from "../../components/AddProduct/AddProduct";
+import ListProduct from "../../components/ListProduct/ListProduct";
 
 const Admin = () => {
   return (
-    <div className='admin'>
+    <div className="admin">
       <Sidebar />
+      <Routes>
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/listproduct" element={<ListProduct />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
