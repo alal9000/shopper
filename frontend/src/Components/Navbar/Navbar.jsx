2e0d1ca3303d8acc3f32671bf62bfaaced7279d2
Aaron Lal <lucidlear@gmail.com>
2024-01-22 19:03:41 +1100

    added auth functionality and data fetching

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,99 @@
-import React, { useContext, useRef, useState } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../Context/ShopContext'
-import dropdown_icon from '../Assets/dropdown_icon.png'
+import React, { useContext, useRef, useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+import dropdown_icon from "../Assets/dropdown_icon.png";
 
 const Navbar = () => {
-
-  const navLinkStyle = { textDecoration: 'none' };
+  const navLinkStyle = { textDecoration: "none" };
 
   const [menu, setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext);
+  const { getTotalCartItems } = useContext(ShopContext);
   const menuRef = useRef();
 
   const dropdown_toggle = (e) => {
-    menuRef.current.classList.toggle('nav-menu-visible');
-    e.target.classList.toggle('open')
-  }
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
 
   return (
-    <div className='navbar'>
-      <div className='nav-logo'>
+    <div className="navbar">
+      <div className="nav-logo">
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <img className='nav-dropdown' onClick={dropdown_toggle} src={dropdown_icon} alt="" />
+      <img
+        className="nav-dropdown"
+        onClick={dropdown_toggle}
+        src={dropdown_icon}
+        alt=""
+      />
       <ul ref={menuRef} className="nav-menu">
-        <li onClick={() => {setMenu("shop")}}><Link style={navLinkStyle} to='/' >Shop</Link> {menu === "shop" ? <hr/> : <></>}</li>
-        <li onClick={() => {setMenu("mens")}}><Link style={navLinkStyle} to='/mens'>Men</Link> {menu === "mens" ? <hr/> : <></>}</li>
-        <li onClick={() => {setMenu("womens")}}><Link style={navLinkStyle} to='/womens'>Women</Link> {menu === "womens" ? <hr/> : <></>}</li>
-        <li onClick={() => {setMenu("kids")}}><Link style={navLinkStyle} to='/kids'>Kids</Link> {menu === "kids" ? <hr/> : <></>}</li>
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={navLinkStyle} to="/">
+            Shop
+          </Link>{" "}
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link style={navLinkStyle} to="/mens">
+            Men
+          </Link>{" "}
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link style={navLinkStyle} to="/womens">
+            Women
+          </Link>{" "}
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={navLinkStyle} to="/kids">
+            Kids
+          </Link>{" "}
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
-        <Link to="/login"><button>Login</button></Link>
-        <Link to="/cart"><img src={cart_icon} alt="" /></Link>
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.replace("/");
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,19 +1,25 @@
-import React, { createContext, useState } from "react";
-import all_product from "../Components/Assets/all_product";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
 
-  for (let index = 0; index < all_product.length + 1; index++) {
+  for (let index = 0; index < 300 + 1; index++) {
     cart[index] = 0;
   }
   return cart;
 };
 
 const ShopContextProvider = (props) => {
+  const [all_product, setAll_Product] = useState([]);
   const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  useEffect(() => {
+    fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => setAll_Product(data));
+  }, []);
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
@@ -27,15 +33,16 @@ const ShopContextProvider = (props) => {
   const getTotalCartAmount = () => {
     let totalAmount = 0;
 
-    for(const item in cartItems) {
+    for (const item in cartItems) {
       if (cartItems[item] > 0) {
-
-        let itemInfo = all_product.find((product) => product.id === Number(item))
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
         totalAmount += itemInfo.new_price * cartItems[item];
       }
     }
     return totalAmount;
-  }
+  };
 
   const getTotalCartItems = () => {
     let totalItem = 0;
@@ -45,11 +52,16 @@ const ShopContextProvider = (props) => {
       }
     }
     return totalItem;
-    
+  };
 
-  }
-
-  const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart
+  };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
